faf30434699784eee437912d5bd1af2a41687996
rayj1993 <418411718@qq.com>
2019-07-25 00:11:25 +0800

    gulp打包环境构建

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+let gulp = require('gulp');
+// let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+// let raname = require('gulp-rename');
+let less = require('gulp-less');
+let cleanCss = require('gulp-clean-css');
+
+gulp.task('js', function () {
+    return gulp.src('src/modules/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/modules/'))
+})
+
+gulp.task('less', function () {
+    return gulp.src('src/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('src/css/'))
+})
+
+gulp.task('css', function () {
+    return gulp.src('src/css/*.css')
+    .pipe(cleanCss())
+    .pipe(gulp.dest('dist/css/'))
+})
+
+ //gulp.series|4.0 依赖
+ //gulp.parallel|4.0 多个依赖嵌套 ,'css'
+
+gulp.task('default', ['js','less','css']);
---
+++ src/modules/cooCell.js
@@ -0,0 +1,9 @@
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
